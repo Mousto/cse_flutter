256fc2b0e1fcdccfaeff6778ad09e269abf9b9c8
Mousto <bediaraye-w@yahoo.fr>
2023-06-03 15:47:13 +0200

    modif de on_boarding_screen.dart

--- lib/src/features/authentication/screens/on_boarding/on_boarding_screen.dart
+++ lib/src/features/authentication/screens/on_boarding/on_boarding_screen.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:liquid_swipe/liquid_swipe.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          LiquidSwipe(
+            pages: [],
+          ),
+        ],
+      ),
+    );
   }
 }
