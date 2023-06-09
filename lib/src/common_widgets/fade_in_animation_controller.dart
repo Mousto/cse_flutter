1ac02d395270b9cc4458c568af4f27edd3d4154d
Mousto <bediaraye-w@yahoo.fr>
2023-06-09 23:53:09 +0200

    Animation de WelcomeScreen.dart

--- lib/src/common_widgets/fade_in_animation_controller.dart
+++ lib/src/common_widgets/fade_in_animation_controller.dart
@@ -7,15 +7,20 @@ class FadeInAnimationController extends GetxController {
 
   RxBool animate = false.obs; //Devient observable
 
-  Future startAnimation() async {
+  Future startSplashAnimation() async {
     await Future.delayed(const Duration(milliseconds: 500));
-
     animate.value = true; //animate.value car la variable est de type RxBool
-    await Future.delayed(const Duration(milliseconds: 5000));
-
+    await Future.delayed(const Duration(milliseconds: 3500));
+    animate.value = false;
+    await Future.delayed(const Duration(milliseconds: 2000));
     Get.to(() =>
         const WelcomeScreen()); //pas besoin de context avec Getx pour la navigation
     // Get.to(() =>
     //     OnBoardingScreen()); //pas besoin de context avec Getx pour la navigation
+  }
+
+  Future startAnimation() async {
+    await Future.delayed(const Duration(milliseconds: 500));
+    animate.value = true; //animate.value car la variable est de type
   }
 }
